c50652617b965faa15d9974600fc1fc0c46fc7af
Marianscabas <mariancabanacabas98@gmail.com>
2023-01-25 02:14:22 -0500

    add logica

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import FiltersTodo from "./FiltersTodo"
+import { Todo } from "./Todo"
+import { TodoFilters } from "./TodoFilters"
 
-
-const TodoList = ({children}) => {
-  return (
-    <div className='flex flex-col mt-7 rounded-lg overflow-hidden shadow-2xl '>
-     {children}
-     {/* <FiltersTodo/> */}
-    </div>
-  )
+const TodoList = ({
+    todos,
+    activeFilter,
+    handleSetComplete,
+    handleDelete,
+    handleClearComplete,
+    showAllTodos,
+    showActiveTodos,
+    showCompletedTodos }) => {
+    return (
+        <div className="flex flex-col mt-7 rounded-lg overflow-hidden shadow-2xl">
+            {todos.map(todo => (
+                <Todo key={todo.id} todo={todo} handleSetComplete={handleSetComplete} handleDelete={handleDelete} />
+            )
+            )}
+            <TodoFilters
+                activeFilter={activeFilter}
+                total={todos.length}
+                showAllTodos={showAllTodos}
+                showActiveTodos={showActiveTodos}
+                showCompletedTodos={showCompletedTodos}
+                handleClearComplete={handleClearComplete} />
+        </div>
+    )
 }
 
-export default TodoList
+export { TodoList }
